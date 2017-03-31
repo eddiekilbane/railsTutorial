2dbadb48b1b547f9e06d777d340eac255385b075
Eddie Kilbane <kilbaneed@outlook.com>
2017-03-31 15:27:29 +0100

    Chptr 10 CRUD operations for User

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,7 +11,12 @@ module SessionsHelper
         cookies.permanent.signed[:user_id] = user.id
         cookies.permanent[:remember_token] = user.remember_token
     end
-    
+
+    # Returns true if the given user is the current user.
+    def current_user?(user)
+        user == current_user
+    end
+
     # Returns the current logged in user (if any)
     def current_user
         if (user_id = session[:user_id])
@@ -37,11 +42,21 @@ module SessionsHelper
         cookies.delete(:remember_token)
     end
 
-
     # Logs out the current user.
     def log_out
         session.delete(:user_id)
         @current_user = nil
+    end
+
+     # Redirects to stored location (or to the default).
+    def redirect_back_or(default)
+        redirect_to(session[:forwarding_url] || default)
+        session.delete(:forwarding_url)
+    end
+
+    # Stores the URL trying to be accessed.
+    def store_location
+        session[:forwarding_url] = request.original_url if request.get?
     end
     
 
